481464bb4396f7dd1b1823f1562260d93dba5392
Miro Svrtan <miro@mirosvrtan.me>
2012-10-27 19:49:21 +0200

    Puppet: Added lamp class (LAMP)

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -32,8 +32,54 @@ class development {
   }
 }
 
+
+class lamp {
+
+  $devPackages = [ "lamp-server^", "php-pear", "php5-xdebug", "php-cache-lite", "php5-memcache", "php5-curl", "php-apc", "phpmyadmin" ]
+  package { $devPackages:
+    ensure => "installed",
+    require => Exec['apt-get update'],
+  }
+
+  exec { 'PEAR':
+    command => 'pear upgrade PEAR',
+  }
+  exec { 'PEAR autodiscover':
+    command => 'pear config-set auto_discover 1',
+  }
+  exec { 'PEAR php unit':
+    command => 'pear install pear.phpunit.de/PHPUnit',
+  }
+  exec { 'PEAR dbunit':
+    command => 'pear install phpunit/DbUnit',
+  }
+  exec { 'PEAR PHPUnit selenium':
+    command => 'pear install phpunit/PHPUnit_Selenium',
+  }
+  exec { 'PEAR discover PHPMD':
+    command => 'pear channel-discover pear.phpmd.org',
+  }
+  exec { 'PEAR PHPMD':
+    command => 'pear install phpmd/PHP_PMD',
+  }
+
+  exec { 'enable apache modules':
+    command => 'a2enmod headers deflate rewrite',
+  }
+  exec { 'Apache restart':
+    command => '/etc/init.d/apache2 restart',
+  }
+}
+
+
+
+
 include system-update
 
 include base-server
 
 include development
+
+include lamp
+
+
